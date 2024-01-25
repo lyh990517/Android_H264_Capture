60a3747de2cc221f32b6f6c18ee7157e52850c4c
lyh990517 <lyh990517@naver.com>
2024-01-25 17:12:00 +0900

    Apply Module

--- app/src/main/java/com/example/myapplication/CaptureManager.kt
+++ app/src/main/java/com/example/myapplication/CaptureManager.kt
@@ -9,47 +9,16 @@ import android.media.projection.MediaProjectionManager
 import android.os.Build
 import android.util.Log
 import android.view.WindowManager
+import com.example.screen_capture.H264EncoderCaptureFactory
 
 object CaptureManager {
-    private lateinit var mediaProjection: MediaProjection
-    private var h264Encoder: H264Encoder? = null
+    private val captureManager = H264EncoderCaptureFactory.create()
     fun startCapture(filePath: String, code: Int, data: Intent, context: Context) {
-        val mediaProjectionManager: MediaProjectionManager =
-            (context.getSystemService(Service.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager)
-        val window by lazy { context.getSystemService(Context.WINDOW_SERVICE) as WindowManager }
-        val display = context.resources.displayMetrics
-        mediaProjectionManager.apply {
-            mediaProjection = getMediaProjection(code, data)
-            mediaProjection.registerCallback(object : MediaProjection.Callback() {
-                override fun onStop() {
-                    super.onStop()
-                    Log.e("MediaProjection","Stop Capture!")
-                }
-            }, null)
-            h264Encoder = H264Encoder(
-                filePath = filePath,
-                width = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) window.currentWindowMetrics.bounds.width() else window.defaultDisplay.width,
-                height = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) window.currentWindowMetrics.bounds.height() else window.defaultDisplay.height,
-                bindSurface = { surface ->
-                    mediaProjection.createVirtualDisplay(
-                        "screen-h264",
-                        display.widthPixels,
-                        display.heightPixels,
-                        display.densityDpi,
-                        DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC,
-                        surface,
-                        null,
-                        null
-                    )
-                }
-            )
-        }
-        h264Encoder?.startEncode()
+        captureManager.startCapture(filePath, code, data, context)
     }
 
     fun stopCapture() {
-        h264Encoder?.stopEncode()
-        mediaProjection.stop()
+        captureManager.stopCapture()
     }
 }
 
