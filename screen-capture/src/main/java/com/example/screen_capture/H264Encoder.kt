e3902f81504c8c1f603efc2aeac61c665c1992e8
lyh990517 <lyh990517@naver.com>
2024-01-29 10:43:00 +0900

    CleanUp Code

--- screen-capture/src/main/java/com/example/screen_capture/H264Encoder.kt
+++ screen-capture/src/main/java/com/example/screen_capture/H264Encoder.kt
@@ -4,116 +4,109 @@ import android.media.MediaCodec
 import android.media.MediaCodecInfo
 import android.media.MediaFormat
 import android.media.MediaMuxer
-import android.os.Environment
-import android.util.Log
 import android.view.Surface
 import java.io.File
+import java.nio.ByteBuffer
 
 internal class H264Encoder(
     private val filePath: String,
     private val width: Int,
     private val height: Int,
     private val bindSurface: ((Surface) -> Unit)? = null,
-    private val colorFormat: Int = MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface,
-) : Thread(("H264-Encoding-Thread")) {
-    private val mediaCodec: MediaCodec =
-        MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_VIDEO_AVC)
+    private val colorFormat: Int = MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface
+) : Thread("H264-Encoding-Thread") {
+
+    private val mediaCodec: MediaCodec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_VIDEO_AVC)
     private lateinit var mediaMuxer: MediaMuxer
     private var trackIndex = -1
     private var muxerStarted = false
 
+    @Volatile
+    private var isStop = true
 
     init {
         initMediaCodec()
     }
 
     private fun initMediaCodec() {
-        val mediaFormat =
-            MediaFormat.createVideoFormat(
-                MediaFormat.MIMETYPE_VIDEO_AVC,
-                width,
-                height
-            )
-        mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 20)
-        mediaFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 30)
-        mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, width * height)
-
-        mediaFormat.setInteger(
-            MediaFormat.KEY_COLOR_FORMAT,
-            colorFormat
-        )
-
-        mediaCodec.configure(
-            mediaFormat,
-            null,
-            null,
-            MediaCodec.CONFIGURE_FLAG_ENCODE
-        )
-
-        bindSurface?.let {
-            val surface = mediaCodec.createInputSurface()
-            it(surface)
+        val mediaFormat = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, width, height).apply {
+            setInteger(MediaFormat.KEY_FRAME_RATE, 20)
+            setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 30)
+            setInteger(MediaFormat.KEY_BIT_RATE, width * height)
+            setInteger(MediaFormat.KEY_COLOR_FORMAT, colorFormat)
         }
+        mediaCodec.configure(mediaFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
+        bindSurface?.invoke(mediaCodec.createInputSurface())
     }
-
-    @Volatile
-    private var isStop = true
 
     override fun run() {
         super.run()
         mediaCodec.start()
-        val info: MediaCodec.BufferInfo = MediaCodec.BufferInfo()
-        val file = File(filePath)
-        file.parentFile?.apply { if (!exists()) mkdir() }
-        if (!file.exists()) file.createNewFile()
-        mediaMuxer = MediaMuxer(file.path, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
+        val info = MediaCodec.BufferInfo()
+        prepareFile(filePath).let { mediaMuxer = MediaMuxer(it, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4) }
+
         try {
             while (!isStop) {
-                var outIndex = mediaCodec.dequeueOutputBuffer(info, 100_000)
-                while (outIndex >= 0) {
-                    val outputBuffer = mediaCodec.getOutputBuffer(outIndex)
-                    if (!muxerStarted) {
-                        val mediaFormat = mediaCodec.outputFormat
-                        trackIndex = mediaMuxer.addTrack(mediaFormat)
-                        mediaMuxer.start()
-                        muxerStarted = true
-                    }
-                    if (info.flags and MediaCodec.BUFFER_FLAG_CODEC_CONFIG != 0) {
-                        info.size = 0
-                    }
-                    if (info.size != 0) {
-                        if (muxerStarted) {
-                            outputBuffer?.position(info.offset)
-                            outputBuffer?.limit(info.offset + info.size)
-                            mediaMuxer.writeSampleData(trackIndex, outputBuffer!!, info)
-                        }
-                    }
-
-                    mediaCodec.releaseOutputBuffer(outIndex, false)
-                    outIndex = mediaCodec.dequeueOutputBuffer(info, 100_000)
-                }
-
+                encodeVideo(info)
             }
         } catch (e: Exception) {
+            // Log and handle exception more specifically if needed
             e.printStackTrace()
         } finally {
-            if (muxerStarted) {
-                mediaMuxer.stop()
-                mediaMuxer.release()
-            }
-            isStop = true
-            mediaCodec.stop()
-            mediaCodec.release()
-
+            tearDown()
         }
+    }
+
+    private fun encodeVideo(info: MediaCodec.BufferInfo) {
+        var outIndex = mediaCodec.dequeueOutputBuffer(info, 100_000)
+        while (outIndex >= 0) {
+            mediaCodec.getOutputBuffer(outIndex)?.let { outputBuffer ->
+                startMuxerIfNeeded(info)
+                writeEncodedDataToMuxer(outputBuffer, info)
+                mediaCodec.releaseOutputBuffer(outIndex, false)
+            }
+            outIndex = mediaCodec.dequeueOutputBuffer(info, 100_000)
+        }
+    }
+
+    private fun prepareFile(filePath: String): String {
+        return File(filePath).apply {
+            parentFile?.takeIf { !it.exists() }?.mkdirs()
+            if (!exists()) createNewFile()
+        }.path
+    }
+
+    private fun startMuxerIfNeeded(info: MediaCodec.BufferInfo) {
+        if (!muxerStarted && info.flags and MediaCodec.BUFFER_FLAG_CODEC_CONFIG != 0) {
+            trackIndex = mediaMuxer.addTrack(mediaCodec.outputFormat).also { _ ->
+                mediaMuxer.start()
+                muxerStarted = true
+            }
+        }
+    }
+
+    private fun writeEncodedDataToMuxer(outputBuffer: ByteBuffer, info: MediaCodec.BufferInfo) {
+        if (info.size != 0 && muxerStarted) {
+            outputBuffer.position(info.offset).limit(info.offset + info.size)
+            mediaMuxer.writeSampleData(trackIndex, outputBuffer, info)
+        }
+    }
+
+    private fun tearDown() {
+        if (muxerStarted) {
+            mediaMuxer.stop()
+            mediaMuxer.release()
+        }
+        mediaCodec.stop()
+        mediaCodec.release()
     }
 
     fun startEncode() {
         isStop = false
         start()
     }
+
     fun stopEncode() {
         isStop = true
     }
-
 }
